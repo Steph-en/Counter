85a302fdba6f22ccfb2168b89042f64e3c27c561
Steph-en <appahstephen9@gmail.com>
2021-02-20 21:21:58 +0000

    Done with script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let addCount = document.querySelector('#addCount');
+let lowerCount = document.querySelector('#lowerCount');
+let counterValue = document.querySelector('#counterValue');
+
+let counter = 0;
+
+addCount.addEventListener
+(
+    "click" , ()=>
+    {
+        counter++;
+        counterValue.innerText = counter;  
+    }
+);
+
+lowerCount.addEventListener
+(
+    "click" , ()=>
+    {
+        counter--;
+        counterValue.innerText = counter;  
+    }
+);
